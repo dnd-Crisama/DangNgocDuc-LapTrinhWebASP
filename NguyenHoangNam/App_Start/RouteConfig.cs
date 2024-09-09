28c6cf591183ce03c398934509cd68a6c86c4901
nguye <nguye@HOANGNAM>
2024-09-09 14:53:45 +0700

    Add project files.

---
+++ NguyenHoangNam/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NguyenHoangNam
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+
+
+            routes.MapRoute(
+                name: "DanhSach_",
+                url: "danh-sach-thanh-vien",
+                defaults: new { controller = "NguyenHoangNam", action = "DanhSach" },
+                namespaces: new string[]
+                {"NguyenDucAnh.Controllers"}
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "NguyenHoangNam", action = "Index", id = UrlParameter.Optional }
+            );
+
+
+
+        }
+    }
+}
---
+++ NguyenHoangNam/Controllers/NguyenHoangNamController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenHoangNam.Controllers
+{
+    public class NguyenHoangNamController : Controller
+    {
+        // GET: NguyenHoangNam
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult FoodterPartial()
+        {
+            return PartialView();
+        }
+    }
+}
